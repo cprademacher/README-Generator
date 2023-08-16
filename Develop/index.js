66c7d47ba553487985a454db2ed11faf0ebe2b08
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-08-16 17:53:56 -0500

    Added more questions

--- Develop/index.js
+++ Develop/index.js
@@ -22,6 +22,18 @@ const questions = [
     }, 
     {
         type: 'input',
+        message: 'Include a table of contents',
+        name: 'contents',
+        validate: function(answer) {
+            if(answer.length < 1) {
+                return console.log("A valid table of contents is required")
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
         message: 'Write a description of your project.',
         name: 'description',
         validate: function(answer) {
@@ -38,7 +50,7 @@ const questions = [
         name: 'installation',
         validate: function(answer) {
             if(answer.length < 1) {
-                return console.log("A valid project description is required.")
+                return console.log("A valid installation is required.")
             } else {
                 return true;
             }
@@ -50,7 +62,7 @@ const questions = [
         name: 'usage',
         validate: function(answer) {
             if(answer.length < 1) {
-                return console.log("A valid project description is required.")
+                return console.log("A valid usage description is required.")
             } else {
                 return true;
             }
@@ -60,12 +72,55 @@ const questions = [
         type: 'input',
         message: 'Did anyone collaborate on this project?  If so, name them: ',
         name: 'collaborators',
+        validate: function(answer) {
+            if(answer.length < 1) {
+                return console.log("A valid collaborator is required (if no one helped, write 'None').")
+            } else {
+                return true;
+            }
+        }
     },
     {
         type: 'list',
         message: 'Choose a license for your project. ',
         choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
         name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Are there any tests included in your project? ',
+        name: 'tests',
+        validate: function(answer) {
+            if(answer.length < 1) {
+                return console.log("A valid response is required (if no tests, write 'none').")
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username. ',
+        name: 'username',
+        validate: function(answer) {
+            if(answer.length < 1) {
+                return console.log("Enter your GitHub username.")
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Enter your email. ',
+        name: 'email',
+        validate: function(answer) {
+            if(answer.length < 1) {
+                return console.log("Enter your email.")
+            } else {
+                return true;
+            }
+        }
     }
 ];
 
@@ -84,5 +139,3 @@ inquirer.prompt(questions).then((data) => {
 });
 // Write node index.js to start the app
 
-// Everything is working except when I 'preview' the readme file it only shows
-// me an object of the data
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,10 @@ function generateMarkdown(data) {
   return `
   # ${data.title}
 
+  ## Table of Contents
+
+  ${data.contents}
+
   ## Description
   
   ${data.description}
@@ -21,6 +25,18 @@ function generateMarkdown(data) {
   ## License Used
   
   ${data.license}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  github.com/${data.username}
+
+  ${data.email}
+
+  Please reach out to me via GitHub or via my email above if you have any further questions!
 `;
 }
 
